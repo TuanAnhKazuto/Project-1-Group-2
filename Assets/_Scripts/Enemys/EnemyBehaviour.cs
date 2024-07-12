3688bb2d07c53f55cf70b8b094277a6a5ebf8a5a
TuanAnhKazuto <tuananhkazuto@gmail.com>
2024-07-12 10:36:28 +0700

    làm xong quái di chuyển + tấn công

--- Assets/_Scripts/Enemys/EnemyBehaviour.cs
+++ Assets/_Scripts/Enemys/EnemyBehaviour.cs
@@ -1,125 +1,65 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Threading;
-using UnityEditor;
 using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour
 {
-    public Transform rayCast;
-    public LayerMask rayCastMask;
-    public float rayCastLength;
-    public float attackDistance;
-    public float moveSpeed;
-    public float timer;
+    [HideInInspector] public float moveSpeed = 2;
+    [SerializeField] private float leftPosition;
+    [SerializeField] private float rightPosition;
+    private int moveDiretion = 1;
 
+    public float timeStopAtk;
 
-    private RaycastHit2D hit;
-    private GameObject target;
-    private Animator animator;
-    private float distance;
-    private bool atkMode;
-    private bool inRange;
-    private bool cooling;
-    private float intTimer;
+    Animator animator;
 
-    void Awake()
+    private void Start()
     {
-        intTimer = timer;
         animator = GetComponent<Animator>();
     }
 
-    void Update()
+    private void Update()
     {
-        if (inRange)
-        {
-            hit = Physics2D.Raycast(rayCast.position, Vector2.left, rayCastLength, rayCastMask);
-            RayCastDebugger();
-        }
-
-        //
-        if(hit.collider != null)
-        {
-            EnemyLogic();
-        }
-        else if(hit.collider == null)
-        {
-            inRange = false;
-        }
-
-        if(inRange == false)
-        {
-            animator.SetBool("attack", false);
-            StopAttack();
-        }
-    }
-
-    
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if(other.gameObject.transform.tag == "Player")
-        {
-            target = other.gameObject;
-            inRange = true;
-        }
-    }
-
-    public void RayCastDebugger()
-    {
-        if (distance > attackDistance)
-        {
-            UnityEngine.Debug.DrawRay(rayCast.position, Vector2.left * rayCastLength, Color.red);
-        }
-        else if(distance < attackDistance)
-        {
-            UnityEngine.Debug.DrawRay(rayCast.position, Vector2.left * rayCastLength, Color.green);
-        }
-    }
-
-    public void EnemyLogic()
-    {
-        distance = Vector2.Distance(transform.position, target.transform.position);
-
-        if(distance > attackDistance)
-        {
-            Move();
-            StopAttack();
-        }
-        else if(attackDistance >= distance && cooling == false)
-        {
-            Attack();
-        }
-
-        if (cooling)
-        {
-            animator.SetBool("attack", false);
-        }
+        Move();
     }
 
     private void Move()
     {
-        if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Attack1"))
-        {
-            Vector2 targetPosition = new Vector2(target.transform.position.x, transform.position.y);
+        transform.Translate(Vector2.right * moveSpeed * moveDiretion * Time.deltaTime);
 
-            transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed *  Time.deltaTime);
+        Vector2 scale = transform.localScale;
+        if(transform.position.x <= leftPosition)
+        {
+            moveDiretion = 1;
+            scale.x = 1;
+        }
+        else if(transform.position.x >= rightPosition)
+        {
+            moveDiretion = -1;
+            scale.x = -1;
+        }
+        transform.localScale = scale;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Attack();
         }
     }
+
     private void Attack()
     {
-        timer = intTimer;
-        atkMode = true;
-
+        moveSpeed = 0;
         animator.SetBool("attack", true);
+        Invoke("StopAttack", timeStopAtk);
     }
+
     private void StopAttack()
     {
-        cooling = false;
-        atkMode = false;
+        moveSpeed = 2;
         animator.SetBool("attack", false);
     }
-    
 }
