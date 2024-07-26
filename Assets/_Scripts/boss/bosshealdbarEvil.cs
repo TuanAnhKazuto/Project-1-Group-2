bb6c13aee7f46e59f97b0df9bc8dbc51b3bd6b89
tathetruong123 <truongttps40870@gmail.com>
2024-07-27 04:26:40 +0700

    mau boss

---
+++ Assets/_Scripts/boss/bosshealdbarEvil.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class BossHealthBar : MonoBehaviour
+{
+    public Slider healthBar; // Thanh máu
+    public float maxHealth = 100f; // Máu tối đa
+    private float currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.maxValue = maxHealth;
+        healthBar.value = maxHealth;
+    }
+
+    public void TakeDamage(float amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth < 0)
+            currentHealth = 0;
+
+        healthBar.value = currentHealth;
+    }
+
+    public void Heal(float amount)
+    {
+        currentHealth += amount;
+        if (currentHealth > maxHealth)
+            currentHealth = maxHealth;
+
+        healthBar.value = currentHealth;
+    }
+}
