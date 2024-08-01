30ec16a2710bf6a8a492e50a717ca4d5b8861029
HuynhCuongg <huynhcuongvan2004@gmail.com>
2024-08-01 18:47:34 +0700

    NPC mới lần đầu làm chưa quen lắm đóa

--- Assets/_Scripts/NPC/NPCController.cs
+++ Assets/_Scripts/NPC/NPCController.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 
 public class NPCController : MonoBehaviour
 {
-        public string storyText = "Đây là câu chuyện khi bạn va chạm với NPC.";
+      
         public GameObject storyUI;
-
-        private void OnCollisionEnter2D(Collision2D collision)
+    private void Start()
+    {
+        storyUI.SetActive(false);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
         {
-            if (collision.gameObject.tag == "Player")
-            {
-                // Hiển thị câu chuyện khi player va chạm với NPC
-                storyUI.GetComponent<TextMeshProUGUI>().text = storyText;
-                storyUI.SetActive(true);
-            }
+            // Hiển thị câu chuyện khi player va chạm với NPC
+            
+            storyUI.SetActive(true);
+            Invoke("RemoveText", 1);
         }
- }
+    }
+
+    private void RemoveText()
+    {
+        storyUI.SetActive(false);
+
+    }
+}
