87180a0be753f5f7b8bdb526e7830029ca1c47f8
TuanAnhKazuto <tuananhkazuto@gmail.com>
2024-08-06 00:34:19 +0700

    code boss final.... cơ mà lỗi v:
    bận đi làm SQL :v
    mai sửa

--- Assets/_Scripts/boss/Bringer of Death/BossBehaviour.cs
+++ Assets/_Scripts/boss/Bringer of Death/BossBehaviour.cs
@@ -13,11 +13,13 @@ public class BossBehaviour : MonoBehaviour
     public float attackDistace;
     public float moveSpeed;
     public float timer;
+    public Transform leftLimit;
+    public Transform rightLimit;
     #endregion
 
     #region Private
     private RaycastHit2D hit;
-    private GameObject target;
+    private Transform target;
     private Animator animator;
     private float distance;
     private bool attackMode;
@@ -34,9 +36,19 @@ public class BossBehaviour : MonoBehaviour
 
     private void Update()
     {
-        if(inRange)
+        if (!attackMode)
         {
-            hit = Physics2D.Raycast(rayCast.position, Vector2.left, rayCastLenght, rayCastMask);
+            Move();
+        }
+
+        if(!InsideofLimits() && !inRange && !animator.GetCurrentAnimatorStateInfo(0).IsName("AttackNormal"))
+        {
+            SelectTarget();
+        }
+
+        if (inRange)
+        {
+            hit = Physics2D.Raycast(rayCast.position, transform.right, rayCastLenght, rayCastMask);
             RayCastDebugger();
         }
 
@@ -55,16 +67,14 @@ public class BossBehaviour : MonoBehaviour
             animator.SetBool("isWalk", false);
             StopAttack();
         }
-        
     }
 
     private void EnemyLogic()
     {
-        distance = Vector2.Distance(transform.position, target.transform.position);
+        distance = Vector2.Distance(transform.position, target.position);
 
         if(distance > attackDistace)
         {
-            Move();
             StopAttack();
         }
         else if(attackDistace >= distance && !cooling)
@@ -74,18 +84,18 @@ public class BossBehaviour : MonoBehaviour
 
         if (cooling)
         {
+            CoolDown();
             animator.SetBool("Attack", false);
         }
     }
 
     void Move()
     {
-        if(attackMode) return;
         animator.SetBool("isWalk", true);
 
         if (!animator.GetCurrentAnimatorStateInfo(0).IsName("AttackNormal"))
         {
-            Vector2  targetPosition = new Vector2(target.transform.position.x, transform.position.y);
+            Vector2  targetPosition = new (target.position.x, transform.position.y);
 
             transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
         }
@@ -99,6 +109,17 @@ public class BossBehaviour : MonoBehaviour
         animator.SetBool("isWalk", false);
         animator.SetBool("Attack", true);
     }
+
+    void CoolDown()
+    {
+        timer -= Time.deltaTime;
+
+        if(timer <= 0 && cooling && attackMode)
+        {
+            cooling = false;
+            timer = intTimer;
+        }
+    }
     private void StopAttack()
     {
         cooling = false;
@@ -110,7 +131,7 @@ public class BossBehaviour : MonoBehaviour
     {
         if(other.gameObject.tag == "Player")
         {
-            target = other.gameObject;
+            target = other.transform;
             inRange = true;
         }
     }
@@ -119,11 +140,53 @@ public class BossBehaviour : MonoBehaviour
     {
         if(distance > attackDistace)
         {
-            Debug.DrawRay(rayCast.position, Vector2.left * rayCastLenght, Color.red);
+            Debug.DrawRay(rayCast.position, transform.right * rayCastLenght, Color.red);
         }
         else if(attackDistace > distance)
         {
-            Debug.DrawRay(rayCast.position, Vector2.left * rayCastLenght, Color.green);
+            Debug.DrawRay(rayCast.position, transform.right * rayCastLenght, Color.green);
         }
+    }
+
+    public void TriggerCooling()
+    {
+        cooling = true;
+    }
+
+    private bool InsideofLimits()
+    {
+        return transform.position.x > leftLimit.position.x && transform.position.x < rightLimit.position.x;
+    }
+
+    private void SelectTarget()
+    {
+        float distanceToLeft = Vector2.Distance(transform.position, leftLimit.position);
+        float distanceToRight = Vector2.Distance(transform.position, rightLimit.position);
+
+        if(distanceToLeft > distanceToRight)
+        {
+            target = leftLimit;
+        }
+        else
+        {
+            target = rightLimit;
+        }
+
+        Flip();
+    }
+
+    private void Flip()
+    {
+        Vector3 rotation = transform.eulerAngles;
+        if(transform.position.x > target.position.x)
+        {
+            rotation.y = 0f;
+        }
+        else
+        {
+            rotation.y = 180f;
+        }
+
+        transform.eulerAngles = rotation;
     }
 }
