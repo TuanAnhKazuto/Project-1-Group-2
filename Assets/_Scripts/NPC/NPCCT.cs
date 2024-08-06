f93080a73861144c1b11cc163ecb2e95e69965fa
HuynhCuongg <huynhcuongvan2004@gmail.com>
2024-08-07 02:34:40 +0700

    NPC Nhiệm Vụ

---
+++ Assets/_Scripts/NPC/NPCCT.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using UnityEngine;
+
+public class NPCCT : MonoBehaviour
+{
+     public GameObject exclamationMark; // dấu chấm than
+     public GameObject questPanel;  //bản nhiệm vụ
+     private bool isPlayerNearby = false;
+
+     void Start()
+     {
+       exclamationMark.SetActive(false); // Ẩn dấu chấm than khi bắt đầu
+       questPanel.SetActive(false); // Ẩn bản nhiệm vụ khi bắt đầu
+     }
+
+     void OnTriggerEnter(Collider other)
+     {
+       if (other.CompareTag("Player"))
+        {
+          exclamationMark.SetActive(true); 
+          isPlayerNearby = true;
+        }
+     }
+
+     void OnTriggerExit(Collider other)
+     {
+        if (other.CompareTag("Player"))
+         {
+           exclamationMark.SetActive(false); // Ẩn dấu chấm than khi người chơi rời xa
+           isPlayerNearby = false;
+           questPanel.SetActive(false); // Ẩn bản nhiệm vụ khi người chơi rời xa
+         }
+     }
+
+     void Update()
+      {
+            // Kiểm tra nếu người chơi bấm phím "N" và đang gần NPC
+        if (isPlayerNearby && Input.GetKeyDown(KeyCode.N))
+          {
+            questPanel.SetActive(!questPanel.activeSelf); // Hiện hoặc ẩn bản nhiệm vụ khi bấm "N"
+          }
+      }
+  }
+  
