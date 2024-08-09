2e9eff64f69a66da11d778857d8386bf088879ae
HAUMEOOOO <haumdps40975@gmail.com>
2024-08-09 09:28:50 +0700

    map4

---
+++ Assets/_Scripts/qua phim.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Kiểm tra xem đối tượng va chạm có tag là "phim2" không
+        if (other.CompareTag("phim2"))
+        {
+            // Chuyển sang cảnh có tên là "phim2"
+            SceneManager.LoadScene("phim2");
+        }
+    }
+}
