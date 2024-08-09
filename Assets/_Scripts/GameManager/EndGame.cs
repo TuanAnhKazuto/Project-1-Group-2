39c4a6a835f38242a9ce24de40528e5b54c3f798
TuanAnhKazuto <tuananhkazuto@gmail.com>
2024-08-09 10:26:47 +0700

    chỉnh sửa scene end by TuanAnh lần 1

--- Assets/_Scripts/GameManager/EndGame.cs
+++ Assets/_Scripts/GameManager/EndGame.cs
@@ -21,7 +21,7 @@ public class EndGame : MonoBehaviour
 
     IEnumerator TheEnd()
     {
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(6f);
         SceneManager.LoadScene(0);
     }
 }
